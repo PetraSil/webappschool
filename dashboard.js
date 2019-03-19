eae26b57a510151f11f0efe99da948cc012def10
Petra <petra.silavuori@gmail.com>
2019-03-19 12:24:17 +0200

    Dashboard
    Dashboard layout getting started.

---
+++ dashboard.js
@@ -0,0 +1,60 @@
+"use strict";
+
+//TOP AT PAGE LOAD
+window.onbeforeunload = () => {
+    window.scrollTo(0, 0);
+}
+
+
+
+//NAVBAR BACKGROUND HANDLING
+const navbar = document.getElementById("navbar_desktop");
+
+const scrollBar = () => {
+    if (window.pageYOffset > 100) {
+        navbar.classList.add("navbar_background");
+    } else {
+        navbar.classList.remove("navbar_background");
+    }
+};
+
+window.addEventListener("scroll", scrollBar);
+
+
+// Mobile menu handling
+let menu_toggle = false;
+
+const menuAction = () => {
+
+    const menu = document.querySelector("#overlay_menu");
+    const menu_lines = document.getElementsByClassName("menu_lines");
+
+    if (!menu_toggle) {
+        menu.style.display = "flex";
+        setTimeout(function () {
+            menu.style.opacity = "1";
+        }, 50);
+        menu_toggle = true;
+        for (let i = 0; i < menu_lines.length; i++) {
+            menu_lines[i].style.backgroundColor = "rgb(233, 233, 233)";
+        }
+        document.getElementById("line1").classList.add("menu_line_effect1");
+        document.getElementById("line3").classList.add("menu_line_effect3");
+        document.getElementById("line2").style.opacity = "0";
+
+    } else {
+        menu.style.opacity = "0";
+        setTimeout(function () {
+            menu.style.display = "none";
+        }, 200);
+        menu_toggle = false;
+        for (let i = 0; i < menu_lines.length; i++) {
+            menu_lines[i].style.backgroundColor = "rgb(32, 32, 32)";
+        }
+        document.getElementById("line1").classList.remove("menu_line_effect1");
+        document.getElementById("line3").classList.remove("menu_line_effect3");
+        document.getElementById("line2").style.opacity = "1";
+    }
+}
+
+document.querySelector("#burger").addEventListener("click", menuAction);
