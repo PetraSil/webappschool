3341ad33f3dd6b2be7ac72d8ef0af26161995922
Arttu Sundell <arttusundell@hotmail.com>
2019-04-26 01:04:41 +0300

    Color picking added, troubles with text visibility when picking black

--- training profile tool/dragnsort.php
+++ training profile tool/dragnsort.php
@@ -3,7 +3,7 @@
 
 <?php  
 
-$listitems=["Warmup","Basic cardio", "Medium intensity cardio", "Low intensity cardio", "Low intensity interval", "High intensity interval", "Insane intensity interval", "Flat zone spinning", "Hill climb spinning"];
+$listitems=["Warmup","Basic cardio", "Medium intensity cardio", "Low intensity interval", "High intensity interval", "Insane intensity interval", "Flat zone spinning", "Hill climb spinning"];
 
 ?>
 
@@ -27,15 +27,34 @@ $listitems=["Warmup","Basic cardio", "Medium intensity cardio", "Low intensity c
     <p>Speed <input type="Radio" name="speed" /></p>
     <p>Cadence <input type="Radio" name="cadence" /></p>
 
-    <p>Color <input type="color" name="age" /></p>
+    <p>Color <input type="color" name="color" /></p>
     
 
     <p><input type="submit" name="submit" value="Submit" /></p>
     </form>
 
     <?php 
-        
+        //pushes the submitted new 
         array_push($listitems, htmlentities($name=$_POST["name"]));
+
+        $color=$_POST["color"];
+        echo $color;
+
+        $colors=array(
+            "Warmup"=>"#88aaaa",
+            "Basic cardio"=>"#ab8989",
+            "Medium intensity cardio"=>"#ab89ab",
+            "Low intensity interval"=>"#abab89",
+            "High intensity interval"=>"#bbddcc",
+            "Insane intensity interval"=>"#debdcd",
+            "Flat zone spinning"=>"#cdbdde",
+            "Hill climb spinning"=>"#446677"
+        );
+
+        $colors[htmlentities($name=$_POST["name"])]=$color;
+
+        print_r($colors);
+
 
     ?>
 
@@ -43,7 +62,7 @@ $listitems=["Warmup","Basic cardio", "Medium intensity cardio", "Low intensity c
         <ul id="pickables" class="list-group-item">
             <?php
             foreach ($listitems as $item){
-                echo "<li draggable='true'> $item</li>\n";
+                echo "<li style='background-color:$colors[$item]'; draggable='true'> $item</li>\n";
             }
              ?>
             
